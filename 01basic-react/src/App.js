b5463f4bd0a4506948e309fc63564504a3217abc
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-12-02 09:06:36 +0500

    basic react learning with simple react and vite

---
+++ 01basic-react/src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World | Shaikh Muhammad Usman</h1>
+      <p>What I leraned today</p>
+       <ul>
+          <li>React App</li>
+          <li>React with vite</li>
+          <li>React docs and manipulaiton</li>
+       </ul>
+        
+    </div>
+  );
+}
+
+export default App;
